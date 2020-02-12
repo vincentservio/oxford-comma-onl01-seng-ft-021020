1f712e0766b33c842f9357d65676fc8ac49b12d1
Vincent Servio <vincentservio@gmail.com>
2020-02-12 00:57:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     elsif array.length == 2  
       return array.join(' and ')
     elsif array.length == 3 
-    return array[0], {array[1] array.join(" and ")
+    return " #array[0] , #{array[1] array.join(" and ")"
       
   #array.join(" and ")
 # binding.pry
