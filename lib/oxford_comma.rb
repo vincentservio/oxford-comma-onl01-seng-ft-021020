a182202222780c9d321c71086f46f815d5a8fd11
Vincent Servio <vincentservio@gmail.com>
2020-02-12 01:15:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1 
+    return "#{array[0]}"
+    elsif array.length == 2  
+      return array.join(' and ')
+    elsif array.length >= 3 
+    
+        new_last_array_item = "and #{array[-1]}"
+    array.pop
+    array.push(new_last_array_item)
+    return array.join(", ")
+  end
 end
+    
+#     return  #array[0] , #{array[1] array.join(" and ")
+      
+#   #array.join(" and ")
+# # binding.pry
+# end
+# end
+# end
