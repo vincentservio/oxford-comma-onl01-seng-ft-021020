6d64125978bbb66cd9ecf721955c0240ad15b468
Vincent Servio <vincentservio@gmail.com>
2020-02-12 00:38:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.length == 1 
     return "#{array[0]}"
     elsif array.legnth == 2  
-      return #{array}.join(',')
+      return array.join(' and ')
   #array.join(" and ")
 # binding.pry
 end
